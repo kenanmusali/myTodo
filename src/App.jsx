025cb385417e616903f4471efa496f2751abd20c
kenanmusali <textme0306@gmail.com>
2024-03-02 09:48:11 -0800

    1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import LoginMenu from './components/LoginMenu';
 
-function App() {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => {
+    return (
+        <div>
+            <LoginMenu />
+        </div>
+    );
+};
 
-export default App
+export default App;
